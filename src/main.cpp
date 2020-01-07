8a9e99350d6d312ec8854c424243694217c4a6a6
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-01-07 21:03:56 +0200

    adds printer array

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@ template <typename T>
 void app(const T &name)
 {
     std::cout << "This is " << name;
-    newLine();
 }
 
 template <typename T>
@@ -81,58 +80,35 @@ void VectorOperations<T>::Update(std::vector<T> &vec)
     {
         auto item = std::chrono::system_clock::now();
         std::time_t date = std::chrono::system_clock::to_time_t(item);
-        // vec.at(it) = static_cast<T>(item);
         // std::cout << typeid(item).name() << " " << typeid(date).name();
         // newLine();
         // vec.at(it) = item;
     }
 }
 
-template <typename T>
-void printarray(T vec)
+template <typename T, typename T2>
+void printarray(T *vec, T2 length)
 {
-    size_t size = sizeof(vec);
-    std::cout << size;
-    newLine();
-    std::cout << sizeof(T);
-    newLine();
-    std::cout << sizeof(vec[0]);
-    newLine();
-    std::cout << vec[0];
-    newLine();
+    for (auto id = 0; id < length; ++id)
+    {
+        std::cout << vec[id];
+    }
 }
 
 int main()
 {
     const std::string name = "Robert";
     app(name);
+    newLine();
     auto now = std::chrono::system_clock::now();
     std::time_t date = std::chrono::system_clock::to_time_t(now);
     std::string utc = std::ctime(&date);
-    // std::cout << utc;
 
     auto stringsize = utc.length();
-    std::cout << stringsize;
-    newLine();
     char sdate[stringsize + 1];
     strcpy(sdate, utc.c_str());
-    std::cout << sizeof sdate;
-    newLine();
-    printarray(sdate);
-    newLine();
-    // std::cout << typeid(utc).name();
-    newLine();
-    VectorOperations<double> vop(6, 'a');
-    // std::cout << vop.Sum(vop.array);
-    newLine();
-    /* 
-    vop.show(vop.array);
-    newLine(); */
-
-    /*  vop.Update(vop.array);
-    newLine();
+    printarray(sdate, stringsize);
+    VectorOperations<char> vop(6, 'a');
     vop.show(vop.array);
     newLine();
-    */
-    return 0;
 }
